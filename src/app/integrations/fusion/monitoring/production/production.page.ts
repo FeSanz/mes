c2411f6e4ccf5e002e444b3d8673cb4b54b11c31
FeSanz <68288843+FeSanz@users.noreply.github.com>
2025-07-29 18:51:18 -0600

    Socket avance y ultimas lecturas

--- src/app/integrations/fusion/monitoring/production/production.page.ts
+++ src/app/integrations/fusion/monitoring/production/production.page.ts
@@ -1,32 +1,39 @@
-import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, HostListener
-} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonButton, IonButtons, IonCard, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonMenuButton, IonRow, IonTitle,
+import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, HostListener, OnDestroy, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {
+  IonButtons,
+  IonCard,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonMenuButton,
+  IonRow,
+  IonTitle,
   IonToolbar
 } from '@ionic/angular/standalone';
-import { Platform } from '@ionic/angular';
+import {Platform} from '@ionic/angular';
 
 import {ApiService} from "../../../../services/api.service";
 import {EndpointsService} from "../../../../services/endpoints.service";
 import {AlertsService} from "../../../../services/alerts.service";
 import {HeightTable} from "../../../../models/tables.prime";
 import {FormatForDisplayFromISO} from "../../../../models/date.format";
-import {addIcons} from "ionicons";
 
-import { Button } from "primeng/button";
-import { IconField } from "primeng/iconfield";
-import { InputIcon } from "primeng/inputicon";
-import { InputText } from "primeng/inputtext";
-import { PrimeTemplate } from "primeng/api";
-import { TableModule } from "primeng/table";
-import { Badge } from "primeng/badge";
-import { Tag } from "primeng/tag";
-import { ProgressBar } from "primeng/progressbar";
+import {Button} from "primeng/button";
+import {IconField} from "primeng/iconfield";
+import {InputIcon} from "primeng/inputicon";
+import {InputText} from "primeng/inputtext";
+import {PrimeTemplate} from "primeng/api";
+import {TableModule} from "primeng/table";
+import {Tag} from "primeng/tag";
+import {ProgressBar} from "primeng/progressbar";
 import {Slider} from "primeng/slider";
 import {WebSocketService} from "../../../../services/web-socket.service";
 import {FloatLabel} from "primeng/floatlabel";
 import {Select} from "primeng/select";
+import {contract} from "ionicons/icons";
 
 
 @Component({
@@ -35,17 +42,17 @@ import {Select} from "primeng/select";
   styleUrls: ['./production.page.scss'],
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonButton,
-    IonCard, IonCol, IonGrid, IonIcon, IonRow, Button, IconField, InputIcon, InputText, PrimeTemplate, TableModule,
-    Badge, Tag, ProgressBar, Slider, FloatLabel, Select]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonCard, IonCol, IonGrid, IonRow, Button, IconField, InputIcon, InputText, PrimeTemplate, TableModule,
+    Tag, ProgressBar, Slider, FloatLabel, Select]
 })
-export class ProductionPage implements OnInit, AfterViewInit  {
+export class ProductionPage implements OnInit, AfterViewInit, OnDestroy  {
   scrollHeight: string = '550px';
-  rowsPerPage: number = 50;
-  rowsPerPageOptions: number[] = [10, 25, 50];
+  rowsPerPage: number = 10;
+  rowsPerPageOptions: number[] = [5, 10, 20];
 
   userData: any = {};
   workOrders: any = { items: [] };
+  lastWorkOrders: any = { items: [] };
   progressValue: number[] = [0, 100];
 
   searchValueWO: string = '';
@@ -79,6 +86,12 @@ export class ProductionPage implements OnInit, AfterViewInit  {
     this.UpdateScrollHeight();
   }
 
+  ngOnDestroy() {
+    /*this.websocket.unsubscribe('workorders-new');
+    this.websocket.unsubscribe('workorders-advance');*/
+    console.log('ngOnDestroy');
+  }
+
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.UpdateScrollHeight();
@@ -100,15 +113,17 @@ export class ProductionPage implements OnInit, AfterViewInit  {
         }));
       }
 
-      this.OnStartSuscription(this.organizationSelected.OrganizationId);
+      this.OnStartSuscriptionNewWO(this.organizationSelected.OrganizationId);
+      this.OnSuscriptionAdvanceWO(this.organizationSelected.OrganizationId);
 
     }).catch(error => {
       console.error('Error al obtener OTs:', error);
     });
   }
 
-  OnStartSuscription(orgId:number){
-    this.websocket.SuscribeById({ organization_id: orgId }, 'workorders', (response) => {
+  OnStartSuscriptionNewWO(orgId:number){
+    const wsType = 'workorders-new';
+    this.websocket.SuscribeById({ organization_id: orgId, typews: wsType }, wsType, (response) => {
       const newWorkOrders = response;
       if (newWorkOrders.items && Array.isArray(newWorkOrders.items)) {
         const newItems = newWorkOrders.items.map((item: any) => ({
@@ -117,6 +132,93 @@ export class ProductionPage implements OnInit, AfterViewInit  {
         }));
 
         this.workOrders.items = [...this.workOrders.items, ...newItems];
+      }
+    }).then((ws) => {
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  OnSuscriptionAdvanceWO(orgId: number) {
+    const wsType = 'workorders-advance';
+    this.websocket.SuscribeById({ organization_id: orgId, typews: wsType }, wsType, (response) => {
+
+      if (response && response.items) {
+        const woAdvanced = response.items;
+
+        // Buscar y actualizar la orden correspondiente
+        const orderIndex = this.workOrders.items.findIndex(
+          (item: any) => item.WorkOrderId === woAdvanced.WorkOrderId
+        );
+
+        if (orderIndex !== -1) {
+          // Actualizar los campos que llegaron por WebSocket
+          const updatedItem = {
+            ...this.workOrders.items[orderIndex],
+            CompletedQuantity: woAdvanced.CompletedQuantity || this.workOrders.items[orderIndex].CompletedQuantity,
+            Status: woAdvanced.Status || this.workOrders.items[orderIndex].Status,
+          };
+
+          // Recalcular el avance con la nueva cantidad completada
+          updatedItem.Advance = this.CalculateAdvance(
+            updatedItem.PlannedQuantity,
+            updatedItem.CompletedQuantity
+          );
+
+          // Crear un nuevo objeto completo usando el  actualizado
+          this.workOrders.items = [
+            ...this.workOrders.items.slice(0, orderIndex),
+            updatedItem,
+            ...this.workOrders.items.slice(orderIndex + 1)
+          ];
+
+          const lastWO ={
+            WorkOrderId: updatedItem.WorkOrderId,
+            WorkOrderNumber: updatedItem.WorkOrderNumber,
+            ItemNumber: updatedItem.ItemNumber,
+            Description: updatedItem.Description,
+            PlannedQuantity: updatedItem.PlannedQuantity,
+            DispatchedQuantity: updatedItem.DispatchedQuantity,
+            CompletedQuantity: updatedItem.CompletedQuantity,
+            Status: updatedItem.Status,
+            ResourceCode: updatedItem.ResourceCode,
+
+            ExecutionDate: woAdvanced.ExecutionDate,
+            Number: woAdvanced.Number,
+            Quantity: woAdvanced.Quantity,
+          }
+
+          // Verificar si la orden ya existe en latestWO
+          const existingLastWO = this.lastWorkOrders.items.findIndex(
+            (item: any) => item.WorkOrderId === lastWO.WorkOrderId
+          );
+
+          if (existingLastWO !== -1) {
+            // Si existe, actualizar la orden existente
+            this.lastWorkOrders.items[existingLastWO] = lastWO;
+          } else {
+            // Si no existe, agregar la nueva orden
+            this.lastWorkOrders.items.push(lastWO);
+          }
+
+          // Ordenar por ExecutionDate (más reciente primero)
+          this.lastWorkOrders.items.sort((a: any, b: any) => {
+            const dateA = new Date(a.ExecutionDate).getTime();
+            const dateB = new Date(b.ExecutionDate).getTime();
+            return dateA - dateB; // Orden ascendente (más viejo primero)
+          });
+
+          // Mantener solo las últimas 5 órdenes
+          if (this.lastWorkOrders.items.length > 5) {
+            this.lastWorkOrders.items = this.lastWorkOrders.items.slice(0, 5);
+          }
+
+          // Crear un nuevo array para forzar la detección de cambios
+          this.lastWorkOrders.items = [...this.lastWorkOrders.items];
+
+        } else {
+          console.warn(`No se encontró la orden con ID: ${woAdvanced.WorkOrderId}`);
+        }
       }
     }).then((ws) => {
     }).catch(err => {
@@ -165,4 +267,5 @@ export class ProductionPage implements OnInit, AfterViewInit  {
   }
 
   protected readonly FormatForDisplayFromISO = FormatForDisplayFromISO;
+  protected readonly contract = contract;
 }
